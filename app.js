7df7aa8783842cb24cd890f2fa40fae8c9a4eb00
ijames07 <james.alex@centrum.cz>
2017-12-31 10:06:48 +0100

    removed fixed diff

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const Proxy = require("./");
 const proxy = new Proxy({
   host: "pool.electroneum.hashvault.pro",
   port: 80,
-  dynamicPool: true,
-  diff: 100
+  dynamicPool: true
 });
 proxy.listen(11111);
